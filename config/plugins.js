531b9f860278c8650fac5cb4567ecf019c55e111
Manish-XD <manishsharma.it1@bvp.edu.in>
2022-08-22 18:02:47 +0530

    connected cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,2 +1,20 @@
 // 127144506775-urbch14t4oqipruivbbeqp5spc9nj45r.apps.googleusercontent.com (Client ID)
 // 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 (Client Secret)
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
